ace368d079b049558444b49956c3cbbc693687de
Fandes <53374622+fandesfyf@users.noreply.github.com>
2020-05-05 19:38:11 +0800

    Add files via upload

---
+++ LAVFilters-0.74.1-x64/install.bat
@@ -0,0 +1,4 @@
+cd /d "%~dp0"
+regsvr32.exe LAVAudio.ax
+regsvr32.exe LAVSplitter.ax
+regsvr32.exe LAVVideo.ax
---
+++ LAVFilters-0.74.1-x64/uninstall.bat
@@ -0,0 +1,4 @@
+cd /d "%~dp0"
+regsvr32.exe /u LAVAudio.ax
+regsvr32.exe /u LAVSplitter.ax
+regsvr32.exe /u LAVVideo.ax
